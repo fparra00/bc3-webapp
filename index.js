289211cac2aed96780791cdf1c45c8d0d246c89d
Fernando <fparraoboe@gmail.com>
2023-11-28 06:49:11 +0100

    new build

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const path = require('path');
 
+const axios = require('axios');
 const PORT = process.env.PORT || 3001;
+
+const client_id = 's2ztlGcammyWFdVA7S4P9HX9cGaPKCk8'
+const secret_id = 'UHFxXAnlp3O4daNs'
 
 const app = express();
 
 // Hacer que node sirva los archivos de nuestro app React
 app.use(express.static(path.resolve(__dirname, 'client/build')));
-
-// Manejar las peticiones GET en la ruta /api
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
 
 /*
 1era Red.: Obtener el codigo de la url
@@ -49,6 +47,7 @@ app.get('/SelectProject/', function (req, res) {
       });
 });
 
+
 // Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
@@ -57,3 +56,50 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto: ${PORT}`);
 });
+
+
+//Llamada a APS para obtener el hub
+function findHubId(tok) {
+  let urlHub = 'https://developer.api.autodesk.com/project/v1/hubs';
+  let confheadersHub = {
+      headers: {
+          Authorization: 'Bearer ' + tok
+      }
+  };
+  axios
+      .get(urlHub, confheadersHub)
+      .then((response) => {
+          const id_hub = response.data['data'][0]['id']
+          getProjects(id_hub, tok)
+      })
+      .catch((error) => {
+          console.error(error);
+      });
+}
+
+//Llamada a APS para obtener todos los proyectos
+function getProjects(id_hub, tok) {
+  const urlProject = `https://developer.api.autodesk.com/project/v1/hubs/${id_hub}/projects`;
+  let confheadersProj = {
+      headers: {
+          Authorization: 'Bearer ' + tok
+      }
+  };
+  axios
+      .get(urlProject, confheadersProj)
+      .then((response) => {
+          attributesArray = response
+              .data
+              .data
+              .map((item) => ({name: item.attributes['name'], id: item.relationships['cost']['data']['id']
+              }));
+
+      })
+      .catch((error) => {
+          console.error(error);
+      });
+  app.get('/api/data', (req, res) => {
+      res.json({attributesArray});
+  });
+
+}
