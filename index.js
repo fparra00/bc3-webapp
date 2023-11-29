e291336b46c9b60f335c5eaaed0968d754f448a1
Fernando <fparraoboe@gmail.com>
2023-11-29 23:51:36 +0100

    redir build

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/SelectProject/', function (req, res) {
         grant_type: 'authorization_code',
         code: code,
         redirect_uri: 'https://parserbc3.azurewebsites.net/SelectProject/'
-        //redirect_uri: 'http://localhost:3001/SelectProject/'
+       // redirect_uri: 'http://localhost:3001/SelectProject/'
     };
     let confheaders = {
         headers: {
